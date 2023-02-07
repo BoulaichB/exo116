e33dc064c2442a1e0d13397901be1a87005ee45d
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-02-07 12:28:35 +0100

    Component Imc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import Imc from './components/imc/imc';
 
 function App() {
+  const people = [
+    {
+      nom: 'Jean',
+      poids: 80,
+      taille: 1.78
+    },
+    {
+      nom: 'Mathieu',
+      poids: 65,
+      taille: 1.74
+    },
+    {
+      nom: 'Mahmoud',
+      poids: 75,
+      taille: 1.80
+    },
+    {
+      nom: 'Yasmin',
+      poids: 60,
+      taille: 1.65
+    }
+  ]
   return (
     <div>
-      <Imc/>
+      {people.map(person => (
+        <Imc _nom={person.nom} _poids={person.poids} _taille={person.taille}/>
+      ))
+      }
     </div>
   );
 }
--- src/components/imc/imc.js
+++ src/components/imc/imc.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './public/css/app.css';
 
-const Imc = () => {
+// # Exo 3
+
+// ## Crée un component :
+//     - IMC
+
+// ### Dans le component IMC, crée une function qui permet de calculer l'IMC avec les paramètres et afficher le résultat
+// * Import dans IMC dans App
+// * crée un tableau d'objet et crée plusieur objet avec comme propriété (nom, taille, poids)
+// * Formule pour l'IMC : poids/taille²
+
+const Imc = ({_nom, _poids, _taille}) => {
   return (
-    <div>imc</div>
+    <div>
+        <div>
+            <h1>IMC de {_nom}</h1>
+            <span><strong>{_poids}</strong>/{_taille}<sup>2</sup> = {_poids / Math.floor(_taille * _taille)}</span>
+        </div>
+    </div>
   )
 }
 
